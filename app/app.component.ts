58e04c3d9c978b5c1c9dfa1188a68f527ed9c29a
sc97958 <daniel.anderson2@bbva.com>
2016-12-14 12:06:13 -0600

    beginning of services section

--- app/app.component.ts
+++ app/app.component.ts
@@ -73,26 +73,22 @@ export class Hero {
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+    <li *ngFor="let hero of heroes" 
+      (click)="onSelect(hero)"
+      [class.selected]="hero === selectedHero">
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
-  <h2>{{hero.name}} details!</h2>
-  <div>
-    <label>id: </label>
-    {{hero.id}}
-  </div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name" />
-  </div>`,
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
 })
 export class AppComponent  { 
 
   title = 'Tour of Heroes';
   heroes = HEROES;
-  hero: Hero = {
-  id:1,
-  name: 'Windstorm'
-};
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
