97c702d6160f5dc41672f8f6127cae735656c384
dcatoday <dcatoday@gmail.com>
2017-01-02 21:01:40 -0600

    Begin Routing Section

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-  const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
+import { HeroService } from './Hero.service';
 
 @Component({
   selector: 'my-app',
+  providers: [HeroService],
   styles: [`
   .selected {
     background-color: #CFD8DC !important;
@@ -82,13 +68,24 @@ export class Hero {
   <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
 })
-export class AppComponent  { 
+export class AppComponent implements OnInit  { 
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+  
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
 }
